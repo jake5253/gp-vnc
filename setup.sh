2e3137e9c5faf7c2bc2843861c349e27b0c9c509
jake5253 <jake.admin@gmail.com>
2021-10-26 09:36:25 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+FROM gitpod/workspace-full-vnc
+
+USER root
+
+# Install dependencies
+sudo apt-get update \
+ && sudo apt-get install -y \
+ build-essential  \
+ libaio-dev \
+ libbluetooth-dev \
+ libbrlapi-dev \
+ libbz2-dev \
+ libcap-dev \
+ libcap-ng-dev \
+ libcurl4-gnutls-dev \
+ libgtk-3-dev \
+ libibverbs-dev \
+ libjpeg8-dev \
+ libncurses5-dev \
+ libnuma-dev \
+ librbd-dev \
+ librdmacm-dev \
+ libsasl2-dev \
+ libsdl1.2-dev \
+ libseccomp-dev \
+ libsnappy-dev \
+ libssh2-1-dev \
+ libvde-dev \
+ libvdeplug-dev \
+ libvte-2.90-dev \
+ libxen-dev \
+ liblzo2-dev \
+ valgrind \
+ xfslibs-dev \
+ libnfs-dev \
+ libiscsi-dev \
+ && sudo apt-get clean && sudo rm -rf /var/cache/apt/* && sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /tmp/*
