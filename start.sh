b4d53ec5f3a6ca2eee3ed46e45c46cb36a0ef461
jake5253 <jake.admin@gmail.com>
2021-10-28 00:06:58 +0000

    fixups

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-[[ -n $(which qemu-system-x86_64 2>/dev/null) ]]  || { echo "QEMU not found. Run setup.sh"; exit; }
+#[[ -n $(which qemu-system-x86_64 2>/dev/null) ]]  || { echo "QEMU not found. Run setup.sh"; exit; }
+[[ -n $(which qemu-system-x86_64 2>/dev/null) ]]  || { sudo apt update; sudo apt install qemu-kvm -y; }
 [[ ! -f ubuntu-16.04.7-server-amd64.iso ]] && wget https://releases.ubuntu.com/16.04/ubuntu-16.04.7-server-amd64.iso
 [[ ! -f disk.img ]] && qemu-img create -f qcow2 disk.img 300G
 [[ ! -f OVMF_CODE.fd ]] && wget https://github.com/foxlet/macOS-Simple-KVM/raw/master/firmware/OVMF_CODE.fd
